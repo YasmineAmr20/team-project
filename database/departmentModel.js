67edab72e65ce811f82a467b1b9a5bd769712469
Your Name <you@example.com>
2023-05-07 19:37:58 +0300

    'divided'

---
+++ database/departmentModel.js
@@ -0,0 +1,15 @@
+import { Schema ,model} from "mongoose";
+const deptSchema = new Schema({
+   Dept_code:
+    {
+        type: Number,
+        required:true  //important to put name to make insert -->name =null xx false
+    },
+    Dept_name:
+    {
+        type: String,
+        required:true // city ==null normal true 
+    }
+},{timestamps:true});
+const deptModel=model('Department',deptSchema);//its name in database
+export default deptModel;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { engine } from 'express-handlebars';
-
+import mongoose from "mongoose";
+import login_router from "./routes/loginRouter.js";
+import dept_router from "./routes/deptRouter.js"
 const app = express();
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 app.use(express.static('public'))
-app.get("/",(req,res)=>{
-    res.render('login',{layout:false})
-}
-)
+mongoose.connect('mongodb://localhost:27017/team_project');//connect to database
+
+app.use("/login",login_router)
+
+app.use("/dept",dept_router)
 
 app.listen(200, () => {
     console.log('Server started on port 200');
